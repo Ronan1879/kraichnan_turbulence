a30e95aa06b268860ad759826b7e33144bb21df1
Ronan Legin <ronan.legin@mail.mcgill.ca>
2020-05-14 11:09:21 -0400

    cleaning code

--- scripts/plot_spectrum.py
+++ scripts/plot_spectrum.py
@@ -1,5 +1,5 @@
 """
-Generate power spectrum of fluid simulation
+Plots power spectrum of fluid simulation
 
 Type in command the name of snapshot file to plot power spectrum
 
@@ -25,32 +25,28 @@ domain = post_tools.build_domain(params, comm=None)
 kx = domain.elements(0)
 ky = domain.elements(1)
 
-dim_kx = kx.shape[0]
-dim_ky = ky.shape[1]
-
-temp_ux = post_tools.load_field(filename, domain, 'ux', 0)
-temp_uy = post_tools.load_field(filename, domain, 'uy', 0)
-ux_coef = np.abs(temp_ux['c'])
-uy_coef = np.abs(temp_uy['c'])
+ux = post_tools.load_field(filename, domain, 'ux', 0)
+uy = post_tools.load_field(filename, domain, 'uy', 0)
+ux_coef = np.abs(ux['c'])
+uy_coef = np.abs(uy['c'])
 
 k = np.array(np.meshgrid(ky,kx))
-
 k = np.sqrt(np.abs(k[0])**2 + np.abs(k[1])**2)
 
-ps = (np.abs(ux_coef)**2 + np.abs(uy_coef)**2)/2
+pspec = (np.abs(ux_coef)**2 + np.abs(uy_coef)**2)/2
 
-n_count,bins = np.histogram(k.flatten(),bins=5*params.N)
+n_count,bins = np.histogram(k.flatten(),bins=params.Npix)
 
-n, bins = np.histogram(k.flatten(),bins=5*params.N,weights=ps.flatten())
+n, bins = np.histogram(k.flatten(),bins=params.Npix,weights=pspec.flatten())
 
-# Remove data points with power below threshold. This arises from plotting issues and is to be fixed
+# Remove data points where the power is below threshold. This arises from plotting issues
 bad_indices = np.where(n < threshold)[0]
 n = np.delete(n,bad_indices)
 n_count = np.delete(n_count,bad_indices)
 bins = np.delete(bins,bad_indices)
 
 # Save power spectrum data
-np.save('ps_data.npy',n)
+np.save('pspec_data.npy',n)
 
 plt.plot(bins[:-1],n/n_count)
 plt.ylabel("$E(k)$",fontsize=14)
@@ -58,4 +54,3 @@ plt.xlabel("$k$",fontsize=14)
 plt.xscale("log")
 plt.yscale("log")
 plt.savefig("spectrum.png",dpi=300)
-plt.show()
