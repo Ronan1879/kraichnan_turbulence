70c852107f070cd3cc264681a7028d649c3449c0
Ronan Legin <ronan.legin@mail.mcgill.ca>
2020-10-14 14:37:36 -0400

    changed code to work with .csv files

--- scripts_v2/plot_cost.py
+++ scripts_v2/plot_cost.py
@@ -3,12 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from parameters import *
 
+filename = 'data.csv'
 
-train_path = "training_costs.npy"
-test_path = "testing_costs.npy"
-
-train_cost = np.load(train_path)
-test_cost = np.load(test_path)
+#epochs = np.loadtxt(filename,delimiter=',',skiprows=1)[:,0]
+train_cost = np.loadtxt(filename,delimiter=',',skiprows=1)[:,1]
+test_cost = np.loadtxt(filename,delimiter=',',skiprows=1)[:,2]
 
 fig, ax = plt.subplots(1,1,figsize=(10,8))
 
@@ -16,8 +15,8 @@ for axis in ['top','bottom','left','right']:
     ax.spines[axis].set_linewidth(3)
 ax.tick_params(direction='out',width=2,length=5,labelsize = 20)
 
-plt.plot(train_cost,label="Training",color='b')
-plt.plot(test_cost,label="Testing",ls='--',color='r')
+plt.plot(train_cost,label="Training",color='b',alpha=0.5)
+plt.plot(test_cost,label="Testing",color='r')
 
 plt.ylabel("Cost",fontsize=20)
 plt.xlabel("Epochs",fontsize=20)
