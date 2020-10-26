7ff53e08fc29f46ae177a97803ea74b02f8de31b
Ronan Legin <ronan.legin@mail.mcgill.ca>
2020-10-25 21:50:49 -0400

    Shortened long for loop argument for plotting across columns

--- scripts_v2/plot_subgrid_stress.py
+++ scripts_v2/plot_subgrid_stress.py
@@ -4,48 +4,76 @@ import unet
 import numpy as np
 import tensorflow as tf
 from parameters import *
-
+from dedalus import public as de
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib as mpl
-
 from misc_functions import deviatoric_part
 from misc_functions import array_of_tf_components
+import xarray
+import fluid_functions as ff
 
+
+def load_data(domain,savenum):
+
+    ux_field = domain.new_field(name = 'ux')
+    uy_field = domain.new_field(name = 'uy')
+    
+    filename = 'filtered/snapshots_s%i.nc' %savenum
+    dataset = xarray.open_dataset(filename)
+    comps = ['xx', 'yy','xy']
+    ux = dataset['ux'].data
+    uy = dataset['uy'].data
+    
+    ux_field['g'] = ux
+    uy_field['g'] = uy
+
+    # Compute inputs from velocity fields
+    du_field = ff.uxuy_derivatives(domain,ux_field,uy_field)
+
+    # Velocity field derivatives
+    du = [du['g'] for du in du_field]
+    # Implicit subgrid stress
+    tau = [dataset['im_t'+c].data for c in comps]
+    # Deviatoric subgrid stress
+    tr_tau = tau[0] + tau[1]
+    tau[0] = tau[0] - tr_tau/2
+    tau[1] = tau[1] - tr_tau/2
+    # Reshape as (batch, *shape, channels)
+    input_set = np.moveaxis(np.array(du), 0, -1)[None]
+    label_set = np.moveaxis(np.array(tau), 0, -1)[None]
+
+    return input_set, label_set 
 
 model = unet.Unet(stacks,stack_width,filters_base,output_channels, **unet_kw)
 checkpoint = tf.train.Checkpoint(net=model)
 
-train_path = 'training_costs.npy'
-output_path = 'subgrid_stress_field.png'
+checkpoint.restore(checkpoint_path + '/best_epoch')#.assert_consumed()
 
-training_costs = np.load(train_path)
+x_basis = de.Fourier('x', N_filter, interval=Bx, dealias=3/2)
+y_basis = de.Fourier('y', N_filter, interval=By, dealias=3/2)
+domain = de.Domain([x_basis, y_basis], grid_dtype=np.float64, comm=None)
 
-best_epoch = np.where(training_costs == np.min(training_costs))[0][0]
+dx = domain.bases[0].Differentiate
+dy = domain.bases[1].Differentiate
 
-best_epoch -= best_epoch % save_interval
+kx = domain.elements(0)
+ky = domain.elements(1)
+k = np.sqrt(kx**2 + ky**2)
 
-restore_path = f"{checkpoint_path}-{best_epoch//save_interval}"
-checkpoint.restore(restore_path)#.assert_consumed()
-print('Restored from {}'.format(restore_path))
+input_set, label_set = load_data(domain,66)
 
-inputs = np.load("inputs.npy")[2]
-labels = np.load("labels.npy")[2]
+tf_input = [tf.cast(input_set,datatype)]
+tf_label = tf.cast(label_set,datatype)
+tf_output = model.call(tf_input)
 
-inputs = inputs[np.newaxis,...]
-labels = labels[np.newaxis,...]
-
-tf_inputs = [tf.cast(inputs,datatype)]
-tf_labels = tf.cast(labels,datatype)
-tf_outputs = model.call(tf_inputs)
-
-tau_pred = deviatoric_part(array_of_tf_components(tf_outputs))
-tau_true = deviatoric_part(array_of_tf_components(tf_labels))
+tau_pred = deviatoric_part(array_of_tf_components(tf_output))
+tau_true = deviatoric_part(array_of_tf_components(tf_label))
 
 fig, ax = plt.subplots(3,4,figsize=(13*4,11*3))
 
-pred_list = [tau_pred[0,0],tau_pred[1,1],tau_pred[1,0]]
-true_list = [tau_true[0,0],tau_true[1,1],tau_true[1,0]]
+pred_list = [tau_pred[0,0][0],tau_pred[1,1][0],tau_pred[1,0][0]]
+true_list = [tau_true[0,0][0],tau_true[1,1][0],tau_true[1,0][0]]
 
 names = [r'$\tau_{xx}$',r'$\tau_{yy}$',r'$\tau_{xy}$']
 
@@ -58,65 +86,21 @@ for a in ax.flatten():
     for axis in ['top','bottom','left','right']:
         a.spines[axis].set_linewidth(3)
 for i in range(3):
-    divider1 = make_axes_locatable(ax[i,0])
-    cax1 = divider1.append_axes('right', size='5%', pad=0.1)
-
-    divider2 = make_axes_locatable(ax[i,1])
-    cax2 = divider2.append_axes('right', size='5%', pad=0.1)
-
-    divider3 = make_axes_locatable(ax[i,2])
-    cax3 = divider3.append_axes('right', size='5%', pad=0.1)
-
-    divider4 = make_axes_locatable(ax[i,3])
-    cax4 = divider4.append_axes('right', size='5%', pad=0.1)
 
     name = names[i]
+    results = [pred_list[i], true_list[i], pred_list[i] - true_list[i], np.abs(pred_list[i] - true_list[i])**2]
 
-    pred_grid = pred_list[i][0]
-    true_grid = true_list[i][0]
-    res_grid = pred_grid - true_grid
-    res_sq_grid = np.abs(res_grid)**2
+    for j in range(4):
+        cax = make_axes_locatable(ax[i,j]).append_axes('right', size='5%', pad=0.1)
+        im1 = ax[i,j].imshow(results[j],cmap = 'RdBu_r',extent = (Bx[0],Bx[1],By[0],By[1]))
+        clb = fig.colorbar(im1, cax = cax, orientation = 'vertical')
+        ax[i,j].set_title(name + " prediction",fontsize = ls + 6, y = 1.02)
+        ax[i,j].tick_params(direction = 'out',width = 2,length = 5,labelsize = ls)
+        im1.set_clim(vmin = -scale*np.max(results[j]),vmax = scale*np.max(results[j]))
+        clb.ax.tick_params(labelsize=ls)
+        ax[i,j].set_xlabel("x",fontsize=ls)
+        ax[i,j].set_ylabel("y",fontsize=ls)
 
-    max_value = np.max(np.abs(true_grid))
-    min_value = -max_value
-
-    im1 = ax[i,0].imshow(pred_grid,cmap='RdBu_r',extent=(Bx[0],Bx[1],By[0],By[1]))
-    clb1 = fig.colorbar(im1, cax=cax1, orientation='vertical')
-    ax[i,0].set_title(name+" prediction",fontsize=ls+6,y=1.02)
-    ax[i,0].tick_params(direction='out',width=2,length=5,labelsize = ls)
-    im1.set_clim(vmin=scale*min_value,vmax=scale*max_value)
-    clb1.ax.tick_params(labelsize=ls)
-    ax[i,0].set_xlabel("x",fontsize=ls)
-    ax[i,0].set_ylabel("y",fontsize=ls)
-
-    im2 = ax[i,1].imshow(true_grid,cmap='RdBu_r',extent=(Bx[0],Bx[1],By[0],By[1]))
-    clb2 = fig.colorbar(im2, cax=cax2, orientation='vertical')
-    ax[i,1].set_title(name+" truth",fontsize=ls+6,y=1.02)
-    ax[i,1].tick_params(direction='out',width=2,length=5,labelsize = ls)
-    im2.set_clim(vmin=scale*min_value,vmax=scale*max_value)
-    clb2.ax.tick_params(labelsize=ls)
-    ax[i,1].set_xlabel("x",fontsize=ls)
-    ax[i,1].set_ylabel("y",fontsize=ls)
-
-    im3 = ax[i,2].imshow(res_grid,cmap='RdBu_r',extent=(Bx[0],Bx[1],By[0],By[1]))
-    clb3 = fig.colorbar(im3, cax=cax3, orientation='vertical')
-    ax[i,2].set_title(name+" residuals",fontsize=ls+6,y=1.02)
-    ax[i,2].tick_params(direction='out',width=2,length=5,labelsize = ls)
-    im3.set_clim(vmin=-scale*np.max(np.abs(res_grid)),vmax=scale*np.max(np.abs(res_grid)))
-    clb3.ax.tick_params(labelsize=ls)
-    ax[i,2].set_xlabel("x",fontsize=ls)
-    ax[i,2].set_ylabel("y",fontsize=ls)
-
-    im4 = ax[i,3].imshow(res_sq_grid,cmap='RdBu_r',extent=(Bx[0],Bx[1],By[0],By[1]))
-    clb4 = fig.colorbar(im4, cax=cax4, orientation='vertical')
-    ax[i,3].set_title(name+" residuals squared",fontsize=ls+6,y=1.02)
-    ax[i,3].tick_params(direction='out',width=2,length=5,labelsize = ls)
-    im4.set_clim(vmin=0,vmax=scale*np.max(np.abs(res_sq_grid)))
-    clb4.ax.tick_params(labelsize=ls)
-    ax[i,3].set_xlabel("x",fontsize=ls)
-    ax[i,3].set_ylabel("y",fontsize=ls)
-
-plt.savefig(output_path,bbox_inches='tight',dpi = 200)
-plt.clf()
+plt.savefig('sst_im.png',bbox_inches='tight')
 
 
--- scripts_v2/plot_subgrid_stress_ps.py
+++ scripts_v2/plot_subgrid_stress_ps.py
@@ -1,67 +1,79 @@
-
 import matplotlib.pyplot as plt
 import unet
 import numpy as np
-import parameters as param
 from parameters import *
 import tensorflow as tf
 from dedalus import public as de
-import post as post_tools
 from misc_functions import deviatoric_part
 from misc_functions import array_of_tf_components
+import xarray
+import fluid_functions as ff
 
+def load_data(domain,savenum):
+
+    ux_field = domain.new_field(name = 'ux')
+    uy_field = domain.new_field(name = 'uy')
+    
+    filename = 'filtered/snapshots_s%i.nc' %savenum
+    dataset = xarray.open_dataset(filename)
+    comps = ['xx', 'yy','xy']
+    ux = dataset['ux'].data
+    uy = dataset['uy'].data
+    
+    ux_field['g'] = ux
+    uy_field['g'] = uy
+
+    # Compute inputs from velocity fields
+    du_field = ff.uxuy_derivatives(domain,ux_field,uy_field)
+
+    # Velocity field derivatives
+    du = [du['g'] for du in du_field]
+    # Implicit subgrid stress
+    tau = [dataset['im_t'+c].data for c in comps]
+    # Deviatoric subgrid stress
+    tr_tau = tau[0] + tau[1]
+    tau[0] = tau[0] - tr_tau/2
+    tau[1] = tau[1] - tr_tau/2
+    # Reshape as (batch, *shape, channels)
+    input_set = np.moveaxis(np.array(du), 0, -1)[None]
+    label_set = np.moveaxis(np.array(tau), 0, -1)[None]
+
+    return input_set, label_set 
 
 def pspec(k,field):
     pspec = (np.abs(field['c'])**2 + np.abs(field['c'])**2)/2
 
-    n_count,bins = np.histogram(k.flatten(),bins=2*param.N)
+    n_count,bins = np.histogram(k.flatten(),bins=2*N_filter)
 
-    n, bins = np.histogram(k.flatten(),bins=2*param.N,weights=pspec.flatten())
+    n, bins = np.histogram(k.flatten(),bins=2*N_filter,weights=pspec.flatten())
 
     return bins, n/n_count
 
 
-model = unet.Unet(stacks,stack_width,filters_base,param.output_channels, **unet_kw)
+model = unet.Unet(stacks,stack_width,filters_base,output_channels, **unet_kw)
 checkpoint = tf.train.Checkpoint(net=model)
 
-train_path = 'training_costs.npy'
-output_path = 'ps.png'
+checkpoint.restore(checkpoint_path + '/best_epoch')#.assert_consumed()
 
-training_costs = np.load(train_path)
-
-best_epoch = np.where(training_costs == np.min(training_costs))[0][0]
-
-best_epoch -= best_epoch % save_interval
-
-restore_path = f"{checkpoint_path}-{best_epoch//save_interval}"
-checkpoint.restore(restore_path)#.assert_consumed()
-print('Restored from {}'.format(restore_path))
-
-inputs = np.load("inputs.npy")[2]
-labels = np.load("labels.npy")[2]
-
-inputs = inputs[np.newaxis,...]
-labels = labels[np.newaxis,...]
-
-tf_inputs = [tf.cast(inputs,datatype)]
-tf_labels = tf.cast(labels,datatype)
-tf_outputs = model.call(tf_inputs)
-
-tau_pred = deviatoric_part(array_of_tf_components(tf_outputs))
-tau_true = deviatoric_part(array_of_tf_components(tf_labels))
-
-x_basis = de.Fourier('x', N, interval=Bx, dealias=3/2)
-y_basis = de.Fourier('y', N, interval=By, dealias=3/2)
+x_basis = de.Fourier('x', N_filter, interval=Bx, dealias=3/2)
+y_basis = de.Fourier('y', N_filter, interval=By, dealias=3/2)
 domain = de.Domain([x_basis, y_basis], grid_dtype=np.float64, comm=None)
-
 
 dx = domain.bases[0].Differentiate
 dy = domain.bases[1].Differentiate
 
 kx = domain.elements(0)
 ky = domain.elements(1)
-
 k = np.sqrt(kx**2 + ky**2)
+
+input_set, label_set = load_data(domain,66)
+
+tf_input = [tf.cast(input_set,datatype)]
+tf_label = tf.cast(label_set,datatype)
+tf_output = model.call(tf_input)
+
+tau_pred = deviatoric_part(array_of_tf_components(tf_output))
+tau_true = deviatoric_part(array_of_tf_components(tf_label))
 
 fig, ax = plt.subplots(1,3,figsize=(10*3,8))
 
@@ -96,7 +108,6 @@ for i in [0,1]:
         plt.legend()
 
         l += 1
-plt.savefig(output_path,bbox_inches='tight')
-plt.clf()
+plt.savefig('ps.png',bbox_inches='tight')
 
 
